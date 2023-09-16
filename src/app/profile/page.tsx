69310c38b70794a8871be3167df75fb52ec23e3e
Yakser <iaksanov2044@mail.ru>
2023-09-16 17:57:47 +0300

    Started working on profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import styles from "./page.module.scss";
+import UserCard from "@/app/components/UserCard";
 
 const Page = () => {
     return (
         <section className={styles.profile}>
             <h2 className={'title'}>Профиль</h2>
+            <UserCard/>
+            <h3 className={styles.profile__subtitle}>Мои новости</h3>
+            {/*    NewsPostWithControls: edit button and delete button */}
+            {/*    news edit page/mode */}
         </section>
     );
 };
